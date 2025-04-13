bbff920729c1bb3e6ebfc0e5a5f937c9ca92720c
Safrid Bhueyan <safridbhueyan>
2025-04-13 21:54:30 +0600

    done fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mango_leaf_disease/view/home_screen.dart';
+import 'package:mango_leaf_disease/view_model/demo.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await ScreenUtil.ensureScreenSize();
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
+  static const double deviceWidth = 402.0;
+  static const double deviceHeight = 874.0;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider<Demo>(create: (_) => Demo())],
+      child: ScreenUtilInit(
+        designSize: const Size(deviceWidth, deviceHeight),
+        minTextAdapt: true,
+        ensureScreenSize: true,
+        builder: (context, child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Ovella',
+            home: HomeScreen(),
+          );
+        },
       ),
-   
     );
   }
 }
-
---
+++ lib/view_model/demo.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+class Demo with ChangeNotifier {
+  int a = 0;
+}
